b85d44c1aac8034f007c0215748fd6f895847966
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-07-31 12:48:42 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[26];
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<n;i++)
+		   a[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+a[i];
+			
+		}
+		System.out.println(sum/n);
+	}
+}
