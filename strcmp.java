4587b9302c1320a0ef220794b45471fba3de9862
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-24 10:33:55 +0530

    Create strcmp.java

---
+++ strcmp.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		if(s1.length()>s2.length())
+		   System.out.println(s1);
+		else
+		   System.out.println(s2);
+	}
+}
